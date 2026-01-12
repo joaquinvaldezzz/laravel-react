177de43449f9a3b8341717316a6d1a8403a50bba
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-12 14:46:01 +0800

    style: format code

--- resources/js/routes/index.ts
+++ resources/js/routes/index.ts
@@ -1,148 +1,151 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../wayfinder'
+import type { RouteDefinition, RouteFormDefinition, RouteQueryOptions } from "./../wayfinder";
+
+import { queryParams } from "./../wayfinder";
+
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-export const home = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: home.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+export const home = (options?: RouteQueryOptions): RouteDefinition<"get"> => ({
+  url: home.url(options),
+  method: "get",
+});
 
 home.definition = {
-    methods: ["get","head"],
-    url: '/',
-} satisfies RouteDefinition<["get","head"]>
+  methods: ["get", "head"],
+  url: "/",
+} satisfies RouteDefinition<["get", "head"]>;
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
+ * @see routes/web.php:6
+ * @route '/'
+ */
 home.url = (options?: RouteQueryOptions) => {
-    return home.definition.url + queryParams(options)
-}
+  return home.definition.url + queryParams(options);
+};
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-home.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: home.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+home.get = (options?: RouteQueryOptions): RouteDefinition<"get"> => ({
+  url: home.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-home.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
-    url: home.url(options),
-    method: 'head',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+home.head = (options?: RouteQueryOptions): RouteDefinition<"head"> => ({
+  url: home.url(options),
+  method: "head",
+});
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-const homeForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: home.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+const homeForm = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: home.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-homeForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: home.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+homeForm.get = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: home.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:6
-* @route '/'
-*/
-homeForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: home.url({
-        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-            _method: 'HEAD',
-            ...(options?.query ?? options?.mergeQuery ?? {}),
-        }
-    }),
-    method: 'get',
-})
+ * @see routes/web.php:6
+ * @route '/'
+ */
+homeForm.head = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: home.url({
+    [options?.mergeQuery ? "mergeQuery" : "query"]: {
+      _method: "HEAD",
+      ...(options?.query ?? options?.mergeQuery ?? {}),
+    },
+  }),
+  method: "get",
+});
 
-home.form = homeForm
+home.form = homeForm;
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-export const about = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: about.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+export const about = (options?: RouteQueryOptions): RouteDefinition<"get"> => ({
+  url: about.url(options),
+  method: "get",
+});
 
 about.definition = {
-    methods: ["get","head"],
-    url: '/about',
-} satisfies RouteDefinition<["get","head"]>
+  methods: ["get", "head"],
+  url: "/about",
+} satisfies RouteDefinition<["get", "head"]>;
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
+ * @see routes/web.php:10
+ * @route '/about'
+ */
 about.url = (options?: RouteQueryOptions) => {
-    return about.definition.url + queryParams(options)
-}
+  return about.definition.url + queryParams(options);
+};
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-about.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: about.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+about.get = (options?: RouteQueryOptions): RouteDefinition<"get"> => ({
+  url: about.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-about.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
-    url: about.url(options),
-    method: 'head',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+about.head = (options?: RouteQueryOptions): RouteDefinition<"head"> => ({
+  url: about.url(options),
+  method: "head",
+});
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-const aboutForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: about.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+const aboutForm = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: about.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-aboutForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: about.url(options),
-    method: 'get',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+aboutForm.get = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: about.url(options),
+  method: "get",
+});
 
 /**
-* @see routes/web.php:10
-* @route '/about'
-*/
-aboutForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
-    action: about.url({
-        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-            _method: 'HEAD',
-            ...(options?.query ?? options?.mergeQuery ?? {}),
-        }
-    }),
-    method: 'get',
-})
+ * @see routes/web.php:10
+ * @route '/about'
+ */
+aboutForm.head = (options?: RouteQueryOptions): RouteFormDefinition<"get"> => ({
+  action: about.url({
+    [options?.mergeQuery ? "mergeQuery" : "query"]: {
+      _method: "HEAD",
+      ...(options?.query ?? options?.mergeQuery ?? {}),
+    },
+  }),
+  method: "get",
+});
 
-about.form = aboutForm
+about.form = aboutForm;
