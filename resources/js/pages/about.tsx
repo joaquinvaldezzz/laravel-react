1f1dbccec255a1a065f93aeb8e7d949a975c7464
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-19 20:41:10 +0800

    feat: remove about page

--- resources/js/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-amber-600">Hello</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
-    </div>
-  );
-}
