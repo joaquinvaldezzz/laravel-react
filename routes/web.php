78c56897cc71fbfa5c1198f6367b7075e50258e3
John Joaquin Valdez <65848689+joaquinvaldezzz@users.noreply.github.com>
2026-01-19 20:41:19 +0800

    feat: rename welcome to index

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,8 @@ use Inertia\Inertia;
 use App\Http\Controllers\PostController;
 
 Route::get("/", function () {
-  return Inertia::render("welcome");
+  return Inertia::render("index");
 })->name("home");
-
-Route::get("/about", function () {
-  return Inertia::render("about");
-})->name("about");
 
 Route::get("/posts", [PostController::class, "index"]);
 Route::post("/posts", [PostController::class, "store"])->name("posts.store");
